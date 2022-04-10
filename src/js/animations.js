4b762e3e46a46083b8259c0232e611c6b17a754e
RafaelDiaz7 <rafaelaarondiaz123@gmail.com>
2022-04-10 09:00:19 -0400

    Fixed form fields animation and added one to the main title.

--- src/js/animations.js
+++ src/js/animations.js
@@ -120,33 +120,22 @@ observer.observe(document.querySelector('.center-ct'));
 
 
 
+
+
 //Event listener to rewrite input.text__input value to empty string
+
 let inputsArray = document.querySelectorAll('.text__input');
 
 
-//Disapearing animation and then rewrite value with anime js
+//Disapearing animation with anime js
 inputsArray.forEach(
 function(elem) {
     elem.addEventListener("focus", 
         function() {
 
-          // Code to move the cursor to the '0' character. 
-          // (Not finished, for safari and chrome is a code and for firefox is another, different API's)
-          /*if (window.getSelection()) {
-            let selObj = window.getSelection();
-            let range  = selObj.getRangeAt(0);
-            console.log(range);
-          }*/
-          /*if (elem.createTextRange) {
-              var part = elem.createTextRange();
-              part.move("character", 0);
-              part.select();
-            } else if (elem.setSelectionRange) {
-              elem.setSelectionRange(0, 0);
-            }
-            elem.focus();*/
-
-          anime({
+         
+         if ( elem.value ==="Nombre" || elem.value === "Apellido" || elem.value === "Email") {
+             anime({
             targets: elem,
             keyframes: [
               {
@@ -161,9 +150,9 @@ function(elem) {
             ],
             duration: 430,
             easing: 'easeInOutCubic'
-          });
-          
-          setTimeout(function() {
+          }); 
+             
+         setTimeout(function() {
             elem.value = '';
             anime({
               targets: elem,
@@ -175,13 +164,14 @@ function(elem) {
                   color: '#999',
                 },
                 {
-                  color: '#444',
+                  color: '#555',
                 },
               ],
               duration: 5,
               easing: 'easeInOutCubic'
             });
           }, 430);
+         }
         
         });
 });
@@ -189,16 +179,23 @@ function(elem) {
 // When focusing out of each input, 
 // put their value again to avoid user forgetting what input its each one 
 inputsArray[0].addEventListener("focusout", function() {
-      inputsArray[0].value = 'Nombre';
+      if (inputsArray[0].value==="") {
+          inputsArray[0].value = "Nombre";
+      }
 })
 
 inputsArray[1].addEventListener("focusout", function() {
-      inputsArray[1].value = 'Apellido';
+      if (inputsArray[1].value==="") {
+        inputsArray[1].value = "Apellido";
+      }
 })
 
 inputsArray[2].addEventListener("focusout", function() {
-      inputsArray[2].value = 'Email';
-})
+      if (inputsArray[2].value==="") {
+        inputsArray[2].value = "Email";
+      }
+
+ })
 
 
 
