7db781ee20dc3faa9adf90c0f061807abb88a3a7
Rafael D Santos <rafaelaarondiaz123@gmail.com>
2022-10-28 13:00:00 -0400

    About content animation (redo)

--- src/js/animations.js
+++ src/js/animations.js
@@ -123,7 +123,7 @@ const observer1 = new IntersectionObserver(entries => {
         keyframes: [
           {
             opacity: 1,
-            translateX: -140,
+            translateX: 300,
           },
         ],
         duration: 1900,
